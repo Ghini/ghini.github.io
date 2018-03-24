e651abce06ab3a311cbf42950fd33a1d8093fbca
mfrasca <mario@anche.no>
2018-03-24 10:18:52 -0500

    don't complay I can't set the date

--- mkpost.sh
+++ mkpost.sh
@@ -4,11 +4,11 @@ title='no title'
 category=software
 picture=abc.png
 
-date=$(date -I $1)
-name=$date-$(echo $title | tr ' ' '-').md
-timestamp=$(date -Iminutes $1 | sed -e 's/T/ /' -e 's/\(...:..\)$/ \1/')
+date=$(date -I $1 2>/dev/null )
+name=$date-$(echo $title 2>/dev/null | tr ' ' '-').md
+timestamp=$(date -Iminutes $1 2>/dev/null | sed -e 's/T/ /' -e 's/\(...:..\)$/ \1/')
 
-cat <<EOF > _posts/$name
+cat <<EOF >> _posts/$name
 ---
 layout: post
 title:  "$title"
